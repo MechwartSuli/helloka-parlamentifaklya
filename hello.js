36d0dc9239ce23ddf2ef3658a4cc6b0c49741731
parlamentifaklya <20.kokenyesi.andor@mechwart.com>
2024-09-05 09:15:13 +0200

    2nd version

--- hello.js
+++ hello.js
@@ -1,3 +1,5 @@
 function hello() {
     return 'Hello World!';
 }
+
+export.module = hello;
--- hello.test.js
+++ hello.test.js
@@ -2,5 +2,3 @@ const hello = require('./hello');
 test('outputs the correct str', () => {
     expect(hello()).toBe('Hello World!');
 });
-
-//asd
